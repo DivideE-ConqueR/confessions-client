2d0c6e0158a4ebb023013d39fb248915cdd0f3c9
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-08-11 12:24:01 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-9xl text-indigo-700">hello world</h1>
     </div>
   );
