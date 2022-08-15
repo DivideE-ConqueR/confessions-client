4d4a88860cc4e5c873e1a56e6be9af66c57a1f8f
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-08-15 13:01:36 +0530

    Create home page

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import Feed from "../components/Feed";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Feed />
+    </div>
+  );
+}
