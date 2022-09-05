9abef6d6e23270f25aaf0c8d32b6bff7fb09b2ce
lucy2512 <mail4parthib@gmail.com>
2022-09-05 12:44:24 +0530

    like-share component added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,18 @@
-import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import {
+  ChatBubbleOvalLeftEllipsisIcon,
+  EllipsisHorizontalIcon,
+  HandThumbUpIcon,
+  ShareIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { RWebShare } from "react-web-share";
 
 export default function Card() {
+  const [likesCount, setLikesCount] = useState(0);
+
+  const increment = () => {
+    setLikesCount(likesCount + 1);
+  };
   return (
     <>
       <div class="rounded overflow-hidden shadow-md bg-white">
@@ -16,7 +28,7 @@ export default function Card() {
               <p className="font-bold text-gray-300">·</p>
               <p className="text-gray-400/70">4m</p>
             </div>
-            <DotsHorizontalIcon className="w-6 text-gray-500" />
+            <EllipsisHorizontalIcon className="w-6 text-gray-500" />
           </div>
           <p class="text-gray-600 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -35,42 +47,26 @@ export default function Card() {
             #winter
           </span>
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-4 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-            </svg>
-            <span>10</span>
+        <div class="px-6 pt-3 pb-4 flex space-x-8 ">
+          <span class="flex ">
+            <HandThumbUpIcon
+              className="h-5 w-7"
+              onClick={increment}
+            ></HandThumbUpIcon>
+            {likesCount != 0 && likesCount}
           </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-            </svg>
-          </span>
+          <ChatBubbleOvalLeftEllipsisIcon className="h-5 w-5"></ChatBubbleOvalLeftEllipsisIcon>
+
+          <RWebShare
+            data={{
+              text: "Web Share - Confessions",
+              url: "http://localhost:3000",
+              title: "Confessions",
+            }}
+            onClick={() => console.log("shared successfully!")}
+          >
+            <ShareIcon className="h-5 w-5"></ShareIcon>
+          </RWebShare>
         </div>
       </div>
     </>
