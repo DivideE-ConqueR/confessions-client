342a8d7c5b363141f28f4233af1556bdadff4680
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-09-06 13:15:10 +0530

    Add new post textarea and post button

--- src/pages/new.js
+++ src/pages/new.js
@@ -2,8 +2,23 @@ import Navbar from "../components/Navbar";
 
 export default function New() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+
+      <div className="p-4 space-y-6">
+        <h1 className="font-medium text-center">
+          Share your secret with the world
+        </h1>
+        <textarea
+          id="message"
+          rows="10"
+          className="p-2.5 w-full text-sm bg-gray-50 rounded-lg border-2 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Your message..."
+        />
+        <button className="w-full p-2.5 text-sm font-medium text-white bg-blue-500 rounded-lg">
+          Post
+        </button>
+      </div>
+    </>
   );
 }
