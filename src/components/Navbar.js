3e1e038ad42982b2395ecf59c923fe0ba6cb02ee
Md Tausif <mdtausifspj2001@gmail.com>
2022-09-06 12:28:46 +0530

    new page added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { PlusIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
@@ -5,7 +6,9 @@ export default function Navbar() {
     <div className="sticky top-0 p-4 flex justify-between items-center bg-white">
       <p className="text-2xl font-dynaPuff font-medium">Confessions</p>
       <div className="flex items-center space-x-6">
-        <PlusIcon className="w-6 h-6 cursor-pointer" />
+        <Link to="/new">
+          <PlusIcon className="w-6 h-6 cursor-pointer" />
+        </Link>
         <div className="bg-slate-400/20 hover:bg-slate-400/30 p-2 rounded-full cursor-pointer">
           <MagnifyingGlassIcon className="w-6 h-6" />
         </div>
---
+++ src/pages/new.js
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+export default function New() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
