9b053b98c88e36d4445df24d9230ec104c9c9e58
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-09-06 13:24:06 +0530

    Add link to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,11 @@ import { PlusIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export default function Navbar() {
   return (
     <div className="sticky top-0 p-4 flex justify-between items-center bg-white">
-      <p className="text-2xl font-dynaPuff font-medium">Confessions</p>
+      <Link to="/">
+        <p className="text-2xl font-dynaPuff font-medium select-none">
+          Confessions
+        </p>
+      </Link>
       <div className="flex items-center space-x-6">
         <Link to="/new">
           <PlusIcon className="w-6 h-6 cursor-pointer" />
