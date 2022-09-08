90b779e7671fbc1e9c4d523cd9677894c9ba30c0
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-09-08 21:55:33 +0530

    Post message

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,6 +1,38 @@
+// import { TagPicker } from "rsuite";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "../api/base";
 import Navbar from "../components/Navbar";
 
 export default function New() {
+  // const data = [
+  //   "Eugenia",
+  //   "Bryan",
+  //   "Linda",
+  //   "Nancy",
+  //   "Lloyd",
+  //   "Alice",
+  //   "Julia",
+  //   "Albert",
+  // ].map((item) => ({
+  //   label: item,
+  //   value: item,
+  //   role: Math.random() > 0.5 ? "Owner" : "Guest",
+  // }));
+  const navigate = useNavigate();
+
+  const [postBody, setPostBody] = useState("");
+
+  const handleClick = async () => {
+    await axios
+      .post("/posts", {
+        postBody: postBody,
+      })
+      .then(() => console.log("posted"))
+      .catch((err) => console.log(err));
+    navigate("/");
+  };
+
   return (
     <>
       <Navbar />
@@ -12,10 +44,26 @@ export default function New() {
         <textarea
           id="message"
           rows="10"
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
           className="p-2.5 w-full text-sm bg-gray-50 rounded-lg border-2 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
           placeholder="Your message..."
         />
-        <button className="w-full p-2.5 text-sm font-medium text-white bg-blue-500 rounded-lg">
+        {/* <div>
+          <TagPicker
+            creatable
+            data={data}
+            style={{ width: 300 }}
+            menuStyle={{ width: 300 }}
+            onCreate={(value, item) => {
+              console.log(value, item);
+            }}
+          />
+        </div> */}
+        <button
+          onClick={handleClick}
+          className="w-full p-2.5 text-sm font-medium text-white bg-blue-500 rounded-lg"
+        >
           Post
         </button>
       </div>
