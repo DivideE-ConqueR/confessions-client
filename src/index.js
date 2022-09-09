cb67f2591cbaf4b42f67e78818d2e1e805272d8b
lucy2512 <mail4parthib@gmail.com>
2022-09-09 21:33:06 +0530

    Display Post Info

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import en from "javascript-time-ago/locale/en";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App";
+import JavascriptTimeAgo from "javascript-time-ago";
+
+JavascriptTimeAgo.locale(en);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "../api/base";
 import Navbar from "../components/Navbar";
-import Feed from "../components/Feed";
+import Card from "../components/Card";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const response = await await axios.get("/posts").then((res) => res.data);
+      setPosts(response);
+      // ...
+    }
+    fetchData();
+  }, []);
+  // await axios.get("/posts").then((response) => setPosts(response.data));
+
   return (
     <div>
       <Navbar />
-      <Feed />
+      <div className="p-4 bg-slate-400/20 flex flex-col space-y-4">
+        {posts.map((post) => (
+          <Card
+            key={post._id}
+            postBody={post.postBody}
+            createdAt={post.createdAt}
+            name={post.name}
+            id={post.postId}
+            likes={post.likes}
+            dislikes={post.dislikes}
+            comments={post.commentsNumber}
+          />
+        ))}
+      </div>
     </div>
   );
 }
