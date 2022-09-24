b4d18b5d0ccd76c2072ce25e449c673576f3e1af
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-09-24 20:33:48 +0530

    Add dark color

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,21 +1,21 @@
-export default function Input({ placeholder, value, onChange, onClick }) {
+export default function Input({ placeholder, value, onChange, onClick, rows }) {
   return (
     <>
       <textarea
         type="textarea"
-        rows="3"
+        rows={rows}
         id="large-input"
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="p-2.5 w-full text-sm bg-gray-50 rounded-lg border-2 border-gray-300 focus:ring focus:ring-green-500 focus:outline-none"
+        className="p-2.5 w-full text-sm bg-gray-50 rounded-lg border-2 border-gray-300 focus:ring focus:ring-gray-600 focus:outline-none"
         placeholder={placeholder}
       />
       <button
         disabled={value.length === 0 ? true : false}
         onClick={value.length > 0 ? onClick : null}
-        className="w-full p-2.5 text-sm font-medium rounded-lg text-white bg-green-500 hover:bg-green-600 focus:ring focus:ring-green-500 focus:outline-none cursor-pointer disabled:bg-green-300 disabled:cursor-not-allowed"
+        className="w-full p-2.5 text-sm font-medium rounded-lg text-white bg-gray-800 hover:bg-gray-600 focus:ring focus:ring-gray-500 focus:outline-none cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed"
       >
-        Submit
+        Post
       </button>
     </>
   );
