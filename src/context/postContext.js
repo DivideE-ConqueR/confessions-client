86ede59242966f041f05fe7a9bb59a518dabfbaa
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-10-13 04:24:14 +0530

    Add likes functionality

--- src/context/postContext.js
+++ src/context/postContext.js
@@ -1,16 +1,19 @@
-import { createContext, useState } from "react";
-import { postLikesGetter, postLikesSetter } from "../utils/localStorage";
+import { createContext, useEffect, useState } from "react";
+import { getPostLikesFromLS, setPostLikesToLS } from "../utils/localStorage";
 
 export const PostContext = createContext(null);
 
 export default function PostProvider({ children }) {
-  const [postLikes, setPostLikes] = useState(postLikesGetter);
+  const [postLikes, setPostLikes] = useState(getPostLikesFromLS);
+
+  useEffect(() => {
+    setPostLikesToLS(postLikes);
+  }, [postLikes]);
 
   const addLike = (id) => {
     if (postLikes.findIndex((post) => post.id === id) === -1) {
       const newPostLikes = [...postLikes, { id, liked: true }];
       setPostLikes(newPostLikes);
-      postLikesSetter(postLikes);
     }
   };
 
