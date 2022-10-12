4918da40dda17091a1c4c30fff097a4b1e857acb
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-10-13 04:21:10 +0530

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import New from "./pages/new";
 import Post from "./pages/post";
+
 export default function App() {
   return (
     <Routes>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import en from "javascript-time-ago/locale/en";
 import { BrowserRouter } from "react-router-dom";
+import JavascriptTimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 import "./styles/index.css";
 import App from "./App";
-import JavascriptTimeAgo from "javascript-time-ago";
+
 import PostProvider from "./context/postContext";
 
 JavascriptTimeAgo.locale(en);
