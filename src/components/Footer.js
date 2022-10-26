20c49050bc2dded9faf113f7de46380ab2af9a8d
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-10-27 04:58:20 +0530

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,20 @@
+import { HeartIcon } from "@heroicons/react/24/solid";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="p-4 text-center">
+      &copy; {year} | Developed with{" "}
+      <HeartIcon className="inline w-5 text-rose-500" /> by{" "}
+      <a
+        className="underline"
+        href="https://github.com/dividee-conquer"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Divide-Conquer
+      </a>
+    </footer>
+  );
 }
