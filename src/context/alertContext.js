4aafad23f21fe71bf6507b0b54ea687de5260cd1
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-12 18:18:10 +0530

    Create Alert context

---
+++ src/context/alertContext.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+import CustomAlert from "../components/CustomAlert";
+
+export const AlertContext = createContext(null);
+AlertContext.displayName = "AlertContext";
+
+export default function AlertProvider({ children }) {
+  const [alertState, setAlertState] = useState({
+    open: false,
+    message: "",
+    severity: "",
+  });
+
+  const openAlert = (props) => {
+    setAlertState({
+      open: true,
+      message: props.message,
+      severity: props.severity,
+    });
+  };
+
+  const closeAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setAlertState({ open: false, message: "", severity: "" });
+  };
+
+  return (
+    <AlertContext.Provider value={{ openAlert }}>
+      {children}
+
+      <CustomAlert
+        open={alertState.open}
+        message={alertState.message}
+        severity={alertState.severity}
+        handleClose={closeAlert}
+      />
+    </AlertContext.Provider>
+  );
+}
