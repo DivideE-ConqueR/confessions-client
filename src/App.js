d746df5a5d28a9bcdc4655ae2adf17fe85730db1
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-12 19:24:22 +0530

    Set uid on app load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getFromLS, setToLS } from "./utils/storage";
+import nanoid from "./config/nanoid";
 import PostProvider from "./context/postContext";
 import AlertProvider from "./context/alertContext";
 import Home from "./pages/home";
@@ -11,6 +14,12 @@ import Custom404 from "./pages/404";
 const queryClient = new QueryClient();
 
 export default function App() {
+  useEffect(() => {
+    if (getFromLS("uid", "val") === "") {
+      setToLS("uid", nanoid());
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AlertProvider>
