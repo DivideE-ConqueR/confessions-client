959eeb62c506530825d02ce1ccad19cfbbbece56
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-12 19:27:10 +0530

    Set ip address on app load

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { getFromLS, setToLS } from "./utils/storage";
+import { getIP } from "./api/services/ip";
+import { getFromLS, setToLS, setToSS } from "./utils/storage";
 import nanoid from "./config/nanoid";
 import PostProvider from "./context/postContext";
 import AlertProvider from "./context/alertContext";
@@ -15,6 +16,8 @@ const queryClient = new QueryClient();
 
 export default function App() {
   useEffect(() => {
+    getIP().then((ip) => setToSS("ip", ip));
+
     if (getFromLS("uid", "val") === "") {
       setToLS("uid", nanoid());
     }
