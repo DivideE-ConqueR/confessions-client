c91fcef770f614a655b2c7387fbb9144f9e83402
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-12 19:17:23 +0530

    Create getter and setter for session storage

--- src/utils/localStorage.js
+++
@@ -1,8 +0,0 @@
-export function getFromLS(key, type = "arr") {
-  const data = type === "arr" ? [] : "";
-  return JSON.parse(window.localStorage.getItem(key)) || data;
-}
-
-export function setToLS(key, value) {
-  window.localStorage.setItem(key, JSON.stringify(value));
-}
