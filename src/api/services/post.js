365feea98bb7fb745e8f3a77225e353fc7854b29
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-12 19:48:26 +0530

    Add createPost api service

--- src/api/services/post.js
+++ src/api/services/post.js
@@ -1,4 +1,5 @@
 import axios from "../axios";
+import { getFromLS, getFromSS } from "../../utils/storage";
 
 const getAllPosts = () => {
   return axios.get("/posts").then((res) => res.data);
@@ -8,4 +9,14 @@ const getPost = (id) => {
   return axios.get(`/posts/${id}`).then((res) => res.data);
 };
 
-export { getAllPosts };
+const createPost = (data) => {
+  return axios
+    .post("/posts", {
+      ...data,
+      "meta.uid": getFromLS("uid", "val"),
+      "meta.ip": getFromSS("ip", "val"),
+    })
+    .then((res) => res.data);
+};
+
+export { getAllPosts, getPost, createPost };
