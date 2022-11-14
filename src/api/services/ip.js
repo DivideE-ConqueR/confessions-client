ff303781cced1510197fd2ef31762a201d4b9358
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-14 07:38:21 +0530

    Catch error in getIP api service

--- src/api/services/ip.js
+++ src/api/services/ip.js
@@ -5,7 +5,8 @@ const getIP = async () => {
     .get(
       `https://geolocation-db.com/json/${process.env.REACT_APP_GEOLOCATION_API_KEY}`
     )
-    .then((res) => res.data.IPv4);
+    .then((res) => res.data.IPv4)
+    .catch((err) => console.error(`${err.code}: - ${err.message}`));
   return ip;
 };
 
