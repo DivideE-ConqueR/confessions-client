772ea0f756305700879234f1ed7e42844680c194
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-21 01:02:29 +0530

    refactor

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+
 export default function Search() {
   return (
     <>
@@ -7,15 +8,15 @@ export default function Search() {
       <form className="px-6">
         <label
           for="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only"
+          className="sr-only mb-2 text-sm font-medium text-gray-900"
         >
           Search
         </label>
         <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <svg
               aria-hidden="true"
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              className="h-5 w-5 text-gray-500 dark:text-gray-400"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -32,13 +33,13 @@ export default function Search() {
           <input
             type="search"
             id="default-search"
-            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500  focus:ring-2 outline-none"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Search"
             required
           />
           <button
             type="submit"
-            className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2  "
+            className="absolute right-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
             Search
           </button>
