0ddd505b86e588b88379bbdd81e77b5e821212dd
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-11-27 16:52:10 +0530

    feat: add basic template

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,50 +1,7 @@
-import Header from "../components/Header";
-
 export default function Search() {
   return (
-    <>
-      <Header />
-
-      <form className="px-6">
-        <label
-          for="default-search"
-          className="sr-only mb-2 text-sm font-medium text-gray-900"
-        >
-          Search
-        </label>
-        <div className="relative">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 text-gray-500 dark:text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-          </div>
-          <input
-            type="search"
-            id="default-search"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Search"
-            required
-          />
-          <button
-            type="submit"
-            className="absolute right-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
-          >
-            Search
-          </button>
-        </div>
-      </form>
-    </>
+    <div className="grid h-screen place-items-center">
+      <h1 className="opacity-80">Coming Soon...</h1>
+    </div>
   );
 }
