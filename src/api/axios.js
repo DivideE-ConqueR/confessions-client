b1f2e30180268ae554d732cf851c1f3868a3cd0d
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-05-07 08:00:34 +0530

    chore: migrate to vite

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default instance;
--- src/api/services/ip.js
+++ src/api/services/ip.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const getIP = async () => {
   const ip = await axios
     .get(
-      `https://geolocation-db.com/json/${process.env.REACT_APP_GEOLOCATION_API_KEY}`
+      `https://geolocation-db.com/json/${
+        import.meta.env.VITE_GEOLOCATION_API_KEY
+      }`
     )
     .then((res) => res.data.IPv4)
     .catch((err) => console.error(`${err.code}: - ${err.message}`));
